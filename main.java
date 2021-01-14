8c9cb0f653eb4e0724159222663c0b68c1220bf6
Sonia <58107179+sunnysunny07@users.noreply.github.com>
2021-01-14 01:11:36 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+package com.appsdeveloperblog.encryption;
+
+public class EncryptPassword {
+    public static void main(String[] args)
+    {
+        String myPassword = "myPassword123";
+        
+        // Generate Salt. The generated value can be stored in DB. 
+        String salt = PasswordUtils.getSalt(30);
+        
+        // Protect user's password. The generated value can be stored in DB.
+        String mySecurePassword = PasswordUtils.generateSecurePassword(myPassword, salt);
+        
+        // Print out protected password 
+        System.out.println("My secure password = " + mySecurePassword);
+        System.out.println("Salt value = " + salt);
+    }
+}
